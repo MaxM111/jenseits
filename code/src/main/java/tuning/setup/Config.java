33941f598003c635c96587dc1a259450b4c7b4c9
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-03-11 13:53:38 +0100

    Add config class

---
+++ code/src/main/java/tuning/setup/Config.java
@@ -0,0 +1,11 @@
+package tuning.setup;
+
+public class Config {
+    public static final String DB_NAME = "postgres";
+    public static final String HOST = "localhost";
+    public static final String POSTGRES_PORT = "5433";
+    public static final String MARIADB_PORT = "3306";
+    public static final String USER = "postgres";
+    public static final String PWD = "postgres";
+    public static final String PATH = "dblp/auth.tsv";
+}
