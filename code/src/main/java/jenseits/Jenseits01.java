d389f85b3a3f8076cdf53630c5e9946a66eafa07
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-03-14 17:43:52 +0100

    add method to fill in values from "Tabelle 1"

--- code/src/main/java/jenseits/Jenseits01.java
+++ code/src/main/java/jenseits/Jenseits01.java
@@ -9,7 +9,9 @@ import jenseits.setup.Database;
 public class Jenseits01 {
     public static void main(String[] args) throws Exception {
         var conn = DB.getConnection(Database.POSTGRESQL);
+        conn.setAutoCommit(true);
         createTable(conn);
+        fillValues(conn);
     }
 
     static void createTable(Connection conn) throws Exception {
@@ -18,5 +20,24 @@ public class Jenseits01 {
         // NOTE: no length for string specified. Example uses single characters, so 10
         // should suffice
         stmt.execute("CREATE TABLE H_toy (a1 VARCHAR(10), a2 VARCHAR(10), a3 INTEGER)");
+    }
+
+    static void fillValues(Connection conn) throws Exception {
+        var prepSql = "INSERT INTO H_toy (a1, a2, a3) VALUES (?, ?, ?)";
+        var prepStmt = conn.prepareStatement(prepSql);
+
+        var a1Column = new String[] { "a", null, null, null };
+        var a2Column = new String[] { "b", "c", null, null };
+        var a3Column = new Integer[] { null, 2, 3, null };
+        for (int i = 0; i < 4; i++) {
+            prepStmt.setString(1, a1Column[i]);
+            prepStmt.setString(2, a2Column[i]);
+            if (a3Column[i] == null) {
+                prepStmt.setNull(3, java.sql.Types.INTEGER);
+            } else {
+                prepStmt.setInt(3, a3Column[i]);
+            }
+            prepStmt.execute();
+        }
     }
 }
