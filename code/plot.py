5b2905156d38910030d2cbfb66ce4979aa8f06d2
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-04-28 11:16:53 +0200

    add third set of plots

--- code/plot.py
+++ code/plot.py
@@ -58,7 +58,7 @@ def partition_representation(df: pd.DataFrame) -> dict[str, pd.DataFrame]:
 
 palette = {
     "Vertical": "#1f77b4",  # blue
-    "Vertical Optimized": "#ff7f0e",  # orange
+    "Vertical Optimized": "#ff7fae",  # pink
     "Vertical Functions": "#1f77b4",  # blue
     "Vertical Functions (Hash Index)": "#ff7f0e",  # orange
     "Vertical Functions (Batch)": "#8a2b91",  # orange
@@ -97,6 +97,7 @@ def plot_queries_1(df, x, title):
         style="query",  # solid vs dashed = Q1 vs Q2
         markers=True,
         dashes=True,
+        palette=palette,
         errorbar=None,
         legend=None,  # type: ignore
     )
@@ -206,6 +207,7 @@ def plot_queries_2(df, x, title):
         hue="representation",  # color = Vertical vs Optimized
         style="query",  # solid vs dashed = Q1 vs Q2
         markers=True,
+        palette=palette,
         dashes=True,
         errorbar=None,
         legend=None,  # type: ignore
@@ -288,3 +290,112 @@ plot_queries_2(d, "sparsity", "Queries vs Sparsity")
 plot_size_2(d, "tupleCount", "Size vs Tuple Count")
 plot_size_2(d, "attributeCount", "Size vs Attribute Count")
 plot_size_2(d, "sparsity", "Size vs Sparsity")
+
+
+def plot_queries_3(df, x, title):
+    plt.figure(figsize=(6, 4))
+
+    # reshape to long format
+    df_long = df.melt(
+        id_vars=["representation", x],
+        value_vars=["queryCount1", "queryCount2"],
+        var_name="query",
+        value_name="count",
+    )
+
+    # nicer labels
+    df_long["query"] = df_long["query"].map(
+        {
+            "queryCount1": "Q1",
+            "queryCount2": "Q2",
+        }
+    )
+
+    ax = sns.lineplot(
+        data=df_long,
+        x=x,
+        y="count",
+        hue="representation",  # color = Vertical vs Optimized
+        style="query",  # solid vs dashed = Q1 vs Q2
+        markers=True,
+        palette=palette,
+        dashes=True,
+        errorbar=None,
+        legend=None,  # type: ignore
+    )
+
+    ax.set_xticks(
+        [2000, 4000, 8000]
+        if x == "tupleCount"
+        else [5, 10, 15]
+        if x == "attributeCount"
+        else [1 - 1 / 2, 1 - 1 / 4, 1 - 1 / 16]
+    )
+
+    legend_elements = [
+        Line2D([0], [0], color=palette["Vertical Optimized"], lw=2, label="Vertical Optimized"),
+        Line2D(
+            [0], [0], color=palette["Vertical Functions (Hash Index)"], lw=2, label="Vertical Functions (Hash Index)"
+        ),
+        Line2D([0], [0], color="black", linestyle="-", label="Query 1"),
+        Line2D([0], [0], color="black", linestyle="--", label="Query 2"),
+    ]
+    ax.legend(handles=legend_elements, title=None)
+
+    plt.title(title)
+    plt.ylabel("Query Count")
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_size_3(df, x, title):
+    plt.figure(figsize=(6, 4))
+
+    ax = sns.lineplot(
+        data=df,
+        x=x,
+        y="tableSize",
+        hue="representation",
+        palette=palette,
+        marker="o",
+        errorbar=None,
+        legend=False,
+    )
+
+    ax.set_xticks(
+        [2000, 4000, 8000]
+        if x == "tupleCount"
+        else [5, 10, 15]
+        if x == "attributeCount"
+        else [1 - 1 / 2, 1 - 1 / 4, 1 - 1 / 16]
+    )
+
+    legend_elements = [
+        Line2D([0], [0], color=palette["Vertical Optimized"], lw=2, label="Vertical Optimized"),
+        Line2D(
+            [0], [0], color=palette["Vertical Functions (Hash Index)"], lw=2, label="Vertical Functions (Hash Index)"
+        ),
+    ]
+
+    ax.legend(handles=legend_elements, title=None)
+
+    plt.title(title)
+    plt.ylabel("Table Size")
+    plt.tight_layout()
+    plt.show()
+
+
+# ---- Vertical Optimized vs Best Vertical Function (Vertical Functions (Hash)) ----
+
+vertical_funcs = ["Vertical Optimized", "Vertical Functions (Hash Index)"]
+d = df[df["representation"].isin(vertical_funcs)]
+
+plot_queries_3(d, "tupleCount", "Queries vs Tuple Count")
+plot_queries_3(d, "attributeCount", "Queries vs Attribute Count")
+plot_queries_3(d, "sparsity", "Queries vs Sparsity")
+
+# Size
+# NOTE: Once again, consider dropping: they have identical sizes, other than hash index, which is minimally more (due to index)
+plot_size_3(d, "tupleCount", "Size vs Tuple Count")
+plot_size_3(d, "attributeCount", "Size vs Attribute Count")
+plot_size_3(d, "sparsity", "Size vs Sparsity")
