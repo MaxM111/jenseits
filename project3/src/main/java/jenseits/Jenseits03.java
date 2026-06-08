e1be125459333c242e9d99f1495253ed486c56e4
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-08 19:43:42 +0200

    try to use dtd when using SAX parser

--- project3/src/main/java/jenseits/Jenseits03.java
+++ project3/src/main/java/jenseits/Jenseits03.java
@@ -1,9 +1,12 @@
 package jenseits;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
 
 import jenseits.setup.*;
 
@@ -14,9 +17,15 @@ public class Jenseits03 implements AutoCloseable {
 
     public static void main(String[] args) throws Exception {
         SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setValidating(true);
+
         SAXParser parser = factory.newSAXParser();
-        DBLPHandler handler = new DBLPHandler();
-        parser.parse("toy_example.txt", handler);
+
+        var reader = parser.getXMLReader();
+        reader.setEntityResolver((publicId, systemId) -> new InputSource(new FileInputStream("dblp.dtd")));
+        reader.setContentHandler(new DBLPHandler());
+
+        reader.parse("toy_example.txt");
     }
 
     Connection conn;
