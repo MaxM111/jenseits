ec0fbb033125ae7e86d1f381f42ace960385f561
Sam Crawford <sam.crawford@stud.plus.ac.at>
2026-06-16 16:22:37 +0200

    increase size of content

--- project3/src/main/java/jenseits/Node.java
+++ project3/src/main/java/jenseits/Node.java
@@ -90,7 +90,7 @@ public class Node {
         var statement = conn.createStatement();
         statement.execute("DROP TABLE IF EXISTS Node, Edge");
         statement.execute(
-                "CREATE TABLE Node (id BIGINT NOT NULL, s_id VARCHAR(50), type VARCHAR(50) NOT NULL, content VARCHAR(512))");
+                "CREATE TABLE Node (id BIGINT NOT NULL, s_id VARCHAR(100), type VARCHAR(50) NOT NULL, content VARCHAR(512))");
         statement.execute("CREATE TABLE Edge (from_ BIGINT NOT NULL, to_ BIGINT NOT NULL)");
         insertSubtreeToEdgeModel(statement);
         statement.close();
